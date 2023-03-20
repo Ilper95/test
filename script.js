c06015d22f65227cebdce4228854a50d61ee140e
Ilper95 <1ilper95@mail.ru>
2023-03-20 19:40:25 +0300

    another commit

--- script.js
+++ script.js
@@ -1,2 +1,61 @@
 // 1 Переменные
+const firstName = 'Ilya' // camelCase
+// const lastName = 'Perevozov' // string
+// const age = 27 // number
+const isProgrammer = true // boolean
+
+const private = 'private'
+const _ = 'some value'
+const $ = 'some'
+
+// const if = 'some' // err
+
+const withNum5 = '5'
+
+// const 5withNum = '5'// err
+
+
+// 2 Мутирование 
+
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+
+// const lastName = prompt('Введите фамилию')
+// alert(firstName + ' ' + lastName)
+
+
+// 3 Операторы
+
+let currentYear = 2023
+const birthYear  = 1995 
+
+// const age = currentYear - birthYear
+
+const a = 10
+const b = 5
+
+let c = 32
+
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(a % b)
+// console.log(++currentYear)
+// console.log(currentYear)
+// console.log(++currentYear)
+// console.log(--currentYear)
+// console.log(currentYear--)
+
+// 4 Типы данных
+
 
